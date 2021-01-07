ef4356cd5b3a45081355f4a622c0366adb1f2650
Russell Matney <russell.matney@gmail.com>
2021-01-06 22:15:55 -0500

    feat: writes clawe 'install' command
    Theoretically, you can call this on the command line. That is, if you
    have clawe installed - for that I recommend looking at the
    clawe.core/install command.

--- src/clawe/core.clj
+++ src/clawe/core.clj
@@ -1,11 +1,29 @@
 (ns clawe.core
   (:require
-   [ralph.defcom :as defcom :refer [defcom]]))
+   [ralphie.install :as install]
+   [ralph.defcom :as defcom :refer [defcom]]
+   [ralphie.sh :as r.sh])
+  )
 
 (defcom hello
   {:name    "hello"
    :handler (fn [_config _parsed]
               (println "Howdy!"))})
+
+;; *file*
+;; io/file
+;; .getParent
+;; (str "/" ~fname)
+
+(defcom install
+  {:name "install"
+   :handler
+   (fn [_config _parsed]
+     (println "Symlinking repo/awesome to your ~/.config/awesome")
+     (install/symlink
+       (r.sh/expand "~/russmatney/clawe/awesome")
+       ;; TODO use ~WDG_CONFIG~ or whatever that thing is
+       (r.sh/expand "~/.config/awesome")))})
 
 (defn -main [& args]
   (apply defcom/run args))
