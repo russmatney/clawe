73227dbae254d98458bf582bf074af0428117107
Russell Matney <russell.matney@gmail.com>
2021-01-08 15:42:28 -0500

    feat: building clawe as an uberscript
    Creates a bin/clawe that executes the /clawe-script.clj with whatever
    arguments. This is expected to be symlinked to your path somewhere.
    
    Updates clawe.core to support building and installing the binary, so
    that rebuilds are a viable clawe command. This should be refactored into
    a useful tool somewhere, as it's the same logic/style that ralphie and
    vapor also use.
    
    Once installed, clawe can now be rebuilt via `clawe rebuild-clawe`.

--- src/clawe/core.clj
+++ src/clawe/core.clj
@@ -1,24 +1,59 @@
 (ns clawe.core
   (:require
-   [ralphie.install :as install]
-   [ralph.defcom :as defcom :refer [defcom]]
+   [clawe.workspaces]
+   [babashka.process :refer [$ check]]
+
+   [ralphie.install :as r.install]
+   [ralphie.util :as r.util]
+   [ralphie.notify :as r.notify]
    [ralphie.sh :as r.sh]
+
+   [ralph.defcom :as defcom :refer [defcom]]
    [clawe.awesome :as awm]))
 
 (defcom hello-cmd
   {:name    "hello"
    :handler (fn [_config _parsed]
-              (println "Howdy!"))})
+              (println "Howdy"))})
 
 (defcom install-cmd
   {:name "install"
    :handler
    (fn [_config _parsed]
      (println "Symlinking repo/awesome to your ~/.config/awesome")
-     (install/symlink
+     (r.install/symlink
        (r.sh/expand "~/russmatney/clawe/awesome")
        ;; TODO use ~WDG_CONFIG~ or whatever that thing is
        (r.sh/expand "~/.config/awesome")))})
+
+(defn build-uberscript []
+  (r.notify/notify "Re-Building Clawe Uberscript")
+  (let [cp (r.util/get-cp (r.sh/expand "~/russmatney/clawe"))]
+    (->
+      ^{:dir (r.sh/expand "~/russmatney/clawe")}
+      ($ bb -cp ~cp -m clawe.core --uberscript clawe-script.clj)
+      check)
+    (r.notify/notify "Clawe Uberscript Rebuilt.")))
+
+;; (defn install-uberjar
+;;   "Writes and chmods a shell script directly to .local"
+;;   []
+;;   (spit "/home/russ/.local/bin/clawe"
+;;         (str "#!/bin/sh
+;; exec bb /home/russ/russmatney/clawe/clawe-script.clj $@"))
+;;   (r.sh/sh "chmod" "+x" "/home/russ/.local/bin/clawe-script"))
+
+(defn build-clawe-uberscript []
+  (build-uberscript)
+  ;; (install-uberjar)
+  ;; (r.install/symlink
+  ;;   (r.sh/expand "~/russmatney/clawe/clawe-script.clj")
+  ;;   "/home/russ/.local/bin/clawe-script")
+  )
+
+(defcom build-clawe
+  {:name    "rebuild-clawe"
+   :handler (fn [_config _parsed] (build-clawe-uberscript))})
 
 (defcom awm-cli-cmd
   {:name     "awm-cli"
