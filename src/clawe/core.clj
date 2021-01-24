20c4fbc148dbd0683dcc93bcc51c71ff35f53d62
Russell Matney <russell.matney@gmail.com>
2021-01-23 20:55:36 -0500

    feat: clawe dwim command
    Setting up some structure for manual suggestions/actions on top of all
    other clawe commands via rofi.

--- src/clawe/core.clj
+++ src/clawe/core.clj
@@ -11,7 +11,8 @@
    [ralphie.rofi :as r.rofi]
 
    [ralph.defcom :as defcom :refer [defcom]]
-   [clawe.awesome :as awm]))
+   [clawe.awesome :as awm]
+   [ralphie.notify :as notify]))
 
 (defcom hello-cmd
   {:name    "hello"
@@ -27,6 +28,31 @@
                              (r.rofi/rofi {:require-match? true
                                            :msg            "Clawe commands"}))]
        (defcom/call-handler cmd config parsed)))})
+
+(defcom dwim-cmd
+  {:name "dwim"
+   :handler
+   (fn [_config parsed]
+     (some->>
+       (concat
+         [{:rofi/label     "Suggest more things here! <small> but don't get distracted </small>"
+           ;; TODO fix this arity thing
+           :rofi/on-select (fn [_] (notify/notify
+                                     "A quick doctor checkup?"
+                                     "Or the time of day?"))}
+          {:rofi/label     "Some Label"
+           :rofi/on-select (fn [_] (notify/notify "Some Action"))}]
+         (->>
+           (defcom/list-commands)
+           (map (partial r.rofi/defcom->rofi parsed))))
+       (r.rofi/rofi {:require-match? true
+                     :msg            "Clawe commands"})
+       ))})
+
+(comment
+  ;; TODO fill in missing arities for defcom functions
+  ;; and clean up this 2 arg bullshit (config/parsed/ugh)
+  (dwim-cmd nil nil))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Install Awesome config
