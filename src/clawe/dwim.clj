42ea3cb6e8e4b0298713968fce830cf2ffdaa978
Russell Matney <russell.matney@gmail.com>
2021-06-27 16:07:59 -0400

    fix: support dwim when on tags without workspaces

--- src/clawe/dwim.clj
+++ src/clawe/dwim.clj
@@ -15,17 +15,17 @@
    (let [wsp (or wsp (workspaces/current-workspace))]
      (->>
        (concat
-         [{:rofi/label     "Create Workspace Client"
-           :rofi/on-select (fn [_]
-                             ;; TODO detect if workspace client is already open
-                             ;; wrap these nil-punning actions-list api
-                             (r.notify/notify "Creating client for workspace")
-                             (wsp.create/create-client wsp))}
+         [(when wsp {:rofi/label     "Create Workspace Client"
+                     :rofi/on-select (fn [_]
+                                       ;; TODO detect if workspace client is already open
+                                       ;; wrap these nil-punning actions-list api
+                                       (r.notify/notify "Creating client for workspace")
+                                       (wsp.create/create-client wsp))})
           {:rofi/label     "Suggest more things here! <small> but don't get distracted </small>"
            :rofi/on-select (fn [_] (r.notify/notify "A quick doctor checkup?"
                                                     "Or the time of day?"))}
 
-          (when (r.git/repo? (workspaces/workspace-repo wsp))
+          (when (and wsp (r.git/repo? (workspaces/workspace-repo wsp)))
             {:rofi/label     "Fetch repo upstream"
              :rofi/on-select (fn [_]
                                (r.notify/notify "Fetching upstream for workspace")
@@ -33,7 +33,8 @@
                                (r.git/fetch (workspaces/workspace-repo wsp)))})]
          (->>
            (defcom/list-commands)
-           (map r.core/defcom->rofi)))))))
+           (map r.core/defcom->rofi)))
+       (remove nil?)))))
 
 (comment
   (->>
@@ -48,7 +49,7 @@
   (let [wsp (workspaces/current-workspace)]
 
     ;; Notify with git status
-    (when (r.git/repo? (workspaces/workspace-repo wsp))
+    (when (and wsp (r.git/repo? (workspaces/workspace-repo wsp)))
       (r.notify/notify (str "Git Status: " (workspaces/workspace-repo wsp))
                        (->>
                          (r.git/status (workspaces/workspace-repo wsp))
