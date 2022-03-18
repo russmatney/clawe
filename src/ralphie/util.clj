7affc428e4602bda16209070bf570f3a31ae8e74
Russell Matney <russell.matney@gmail.com>
2022-03-18 18:29:39 -0400

    fix: remove dead funcs

--- src/ralphie/util.clj
+++ src/ralphie/util.clj
@@ -1,13 +1,6 @@
 (ns ralphie.util
   (:require
-   [babashka.process :refer [$ check]]
-   [clojure.set :as set]))
-
-
-(defn ensure-set [s] (if set? s (set s)))
-
-(defn matching-ks? [s1 s2]
-  (set/subset? s2 s1))
+   [babashka.process :refer [$ check]]))
 
 (defn get-cp
   "Builds a classpath in a directory."
