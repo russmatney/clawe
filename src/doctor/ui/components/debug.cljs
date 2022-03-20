a882d2bb85fd311fb5235cb350d747281b9d2767
Russell Matney <russell.matney@gmail.com>
2022-03-20 16:39:36 -0400

    feat: colorized and recursive metadata

--- src/doctor/ui/components/debug.cljs
+++ src/doctor/ui/components/debug.cljs
@@ -3,13 +3,51 @@
    [uix.core.alpha :as uix]))
 
 
+(defn colorized-metadata
+  ([m] (colorized-metadata 0 m))
+  ([level m]
+   (cond
+     (map? m)
+     (->> m (map (partial colorized-metadata (inc level))))
+
+     :else
+     (let [[k v] m]
+       (println "k" k "v" v)
+       ^{:key k}
+       [:div.font-mono
+        {:class ["text-city-gray-400" (str "pl-" (* 2 level))]}
+        "["
+        [:span {:class ["text-city-pink-400"]} (str k)]
+        " "
+
+        (cond
+          (and (map? v) (seq v))
+          (->> v (map (partial colorized-metadata (inc level))))
+
+          (and (list? v) (seq v))
+          (->> v (map (partial colorized-metadata (inc level))))
+
+          :else
+          [:span {:class ["text-city-green-400"]}
+           (cond
+             (and (map? v) (empty? v))  "{}"
+             (and (list? v) (empty? v)) "[]"
+
+             (nil? v) "nil"
+
+             :else
+             (str v))])
+
+        "] "]))))
+
+
 (defn raw-metadata
   ([metadata] [raw-metadata nil metadata])
   ([{:keys [label initial-show?]} metadata]
    (let [label                    (or label "Show raw metadata")
          show-raw-metadata?       (uix/state initial-show?)
          toggle-show-raw-metadata #(swap! show-raw-metadata? (comp boolean not))]
-     [:div.text-right
+     [:div
       [:span.text-sm
        {:class    ["hover:text-city-pink-400" "cursor-pointer"]
         :on-click toggle-show-raw-metadata}
@@ -21,6 +59,4 @@
          (when metadata
            (->>
              metadata
-             (map (fn [[k v]]
-                    ^{:key k}
-                    [:div.font-mono "[" (str k) " " (str v) "] "]))))])])))
+             (map colorized-metadata)))])])))
