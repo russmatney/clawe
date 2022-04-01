7172f518d557400eae61cf6937e22301bc91fa44
Russell Matney <russell.matney@gmail.com>
2022-04-01 01:20:02 -0400

    feat: add malli, initial schema toying

--- src/doctor/api/workspaces.clj
+++ src/doctor/api/workspaces.clj
@@ -2,8 +2,11 @@
   (:require
    [systemic.core :refer [defsys] :as sys]
    [manifold.stream :as s]
+   [malli.transform :as mt]
+   [malli.core :as m]
    [clawe.workspaces :as clawe.workspaces]
-   [doctor.util]))
+   [doctor.util]
+   ))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Active workspaces
@@ -28,10 +31,57 @@
   (s/put! *workspaces-stream* (active-workspaces)))
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def fe-workspace
+  "A watered-down workspace, without functions and lists"
+  [:map
+   ;; [:rules/is-my-client? fn?]
+   ;; [:awesome/rules ]
+   [:workspace/readme string?]
+   [:defthing.core/registry-key keyword?]
+   [:ns string?]
+   [:name string?]
+   [:type keyword?]
+   [:git/repo string?]
+   [:awesome.tag/selected boolean?]
+   [:workspace/initial-file string?]
+   [:workspace/updated-at int?] ;; TODO time
+   [:workspace/scratchpad boolean?]
+   [:workspace/directory string?]
+   [:workspace/exec string?] ;; TODO or tmux/fire map?
+   [:awesome.tag/index int?]
+   [:db/id int?]
+   [:workspace/scratchpad-class string?]
+   [:awesome.tag/layout string?]
+   ;; [:awesome.tag/clients ]
+   [:awesome.tag/name string?]
+   [:workspace/title string?]
+   ;; [:awesome/tag ]
+   [:awesome.tag/empty boolean?]
+   [:awesome.tag/urgent boolean?]])
+
+
 (comment
+
+  (m/decode
+    fe-workspace
+    -w
+    (mt/strip-extra-keys-transformer))
+
+
   (->>
     (active-workspaces)
     (sort-by :awesome.tag/index)
     first)
 
-  (update-workspaces))
+  (update-workspaces)
+
+
+  (def -w
+    (->> (clawe.workspaces/all-workspaces)
+         (filter :awesome.tag/name)
+         first
+         ))
+
+  )
