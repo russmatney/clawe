a41c2822a9dd8f2b39e497eaa375e98c37254817
Russell Matney <russell.matney@gmail.com>
2022-05-01 14:21:29 -0400

    feat: add expo as known endpoints, sets default shadow opts

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,8 +3,11 @@
 
 ((clojure-mode
   (cider-clojure-cli-aliases . "dev")
-  (cider-known-endpoints . (("doctor-clj-server" "localhost" "3336")
-                            ("doctor-cljs-shadow" "localhost" "3335")))
+  (cider-known-endpoints
+   . (("expo-clj-server" "localhost" "4442")
+      ("expo-cljs-shadow" "localhost" "3335")
+      ("doctor-clj-server" "localhost" "3336")
+      ("doctor-cljs-shadow" "localhost" "3335")))
   (cider-clojure-cli-global-options . "-A:doctor-deps:dev:test")
   (cider-preferred-build-tool . shadow-cljs)
   (cider-jack-in-default . shadow-cljs)
@@ -13,8 +16,11 @@
   (cider-shadow-cljs-global-options . "-A:doctor-deps:dev")
   (company-css-classes-filepath . "~/russmatney/clawe/public/css/main-built.css"))
  (clojurec-mode
-  (cider-known-endpoints . (("doctor-clj-server" "localhost" "3336")
-                            ("doctor-cljs-shadow" "localhost" "3335")))
+  (cider-known-endpoints
+   . (("expo-clj-server" "localhost" "4442")
+      ("expo-cljs-shadow" "localhost" "3335")
+      ("doctor-clj-server" "localhost" "3336")
+      ("doctor-cljs-shadow" "localhost" "3335")))
   (cider-clojure-cli-global-options . "-A:doctor-deps:dev:test")
   (cider-preferred-build-tool . shadow-cljs)
   (cider-jack-in-default . shadow-cljs)
@@ -23,11 +29,13 @@
   (cider-shadow-cljs-global-options . "-A:doctor-deps:dev")
   (company-css-classes-filepath . "~/russmatney/clawe/public/css/main-built.css"))
  (clojurescript-mode
-  (cider-known-endpoints . (("doctor-cljs-shadow" "localhost" "3335")))
+  (cider-known-endpoints
+   . (("expo-cljs-shadow" "localhost" "3335")
+      ("doctor-cljs-shadow" "localhost" "3335")))
   (cider-clojure-cli-global-options . "-A:doctor-deps:dev:test")
   (cider-preferred-build-tool . shadow-cljs)
   (cider-jack-in-default . shadow-cljs)
   (cider-default-cljs-repl . shadow)
-  (cider-shadow-default-options . ":doctor-app")
+  (cider-shadow-default-options . ":expo-app") ;; ":doctor-app"?
   (cider-shadow-cljs-global-options . "-A:doctor-deps:dev")
   (company-css-classes-filepath . "~/russmatney/clawe/public/css/main-built.css")))
