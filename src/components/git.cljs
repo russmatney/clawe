170fbad27da925fb81bfa44b5e2bee68d3acc80b
Russell Matney <russell.matney@gmail.com>
2022-06-25 18:46:04 -0400

    chore: delete dead component

--- src/components/git.cljs
+++ src/components/git.cljs
@@ -1,6 +1,5 @@
 (ns components.git
   (:require
-   [clojure.string :as string]
    [tick.core :as t]
 
    [components.debug]
@@ -12,11 +11,6 @@
 (declare repo-label)
 (declare repo-popover-content)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; commits
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
 (defn short-repo [it]
   (some->>
     (or (:git.commit/directory it)
@@ -25,58 +19,9 @@
     first
     first))
 
-(defn commit-comp [_opts it]
-  (let [{:git.commit/keys
-         [body subject short-hash hash
-          lines-added lines-removed
-          ]}      it
-        short-dir (short-repo it)
-        body-lines
-        (->
-          body
-          (string/split "\n"))]
-    [:div
-     {:class ["flex" "flex-col"]}
-
-     [:div
-      {:class ["flex" "flex-row"]}
-      [:div
-       {:class ["text-xl" "text-city-green-400"]}
-       subject]
-
-      [:div
-       {:class ["flex" "flex-col" "ml-auto"]}
-       [:a {:class ["text-city-pink-400"
-                    "hover:text-city-pink-200"
-                    "hover:cursor-pointer"]
-            ;; TODO not all commits have public repos (ignore dropbox)
-            ;; TODO include this link in db commits
-            :href  (str "https://github.com/" short-dir "/commit/" hash)}
-        short-hash]
-
-       [:a {:class ["text-city-pink-400"
-                    "hover:text-city-pink-200"
-                    "hover:cursor-pointer"]
-            :href  (str "https://github.com/" short-dir)}
-        short-dir]
-
-       [:div
-        {:class ["text-city-green-200"]}
-        (str "+" lines-added " added")]
-
-       [:div
-        {:class ["text-city-red-200"]}
-        (str "-" lines-removed " removed")]]]
-
-     [:div
-      {:class [(when (seq body-lines) "pt-8")]}
-      (for [[idx line] (->> body-lines
-                            (map-indexed vector))]
-        ^{:key idx}
-        [:p line])]]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; thumbnail
+;; commit thumbnail
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn commit-thumbnail
@@ -96,11 +41,11 @@
      (:git.commit/subject commit)]]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; popover
+;; commit-popover
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn commit-popover
-  ([commit] [commit-popover nil commit])
+(defn commit-popover-content
+  ([commit] [commit-popover-content nil commit])
   ([_opts commit]
    (let [repo-name (short-repo commit)
          repo      (:commit/repo commit)]
@@ -201,7 +146,7 @@
            [components.git/commit-thumbnail opts event]]
           :popover-comp
           [:div
-           [components.git/commit-popover opts event]]}])])])
+           [components.git/commit-popover-content opts event]]}])])])
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; repos
