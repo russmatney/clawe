ac82e6758497564ba677032f5548799b1c3e4411
Russell Matney <russell.matney@gmail.com>
2022-07-30 12:43:08 -0400

    wip: querying org notes with filename regex

--- src/expo/core.clj
+++ src/expo/core.clj
@@ -20,12 +20,46 @@
               tags ignored-tags)
     (map first)))
 
+(defn notes-matching-source-file-reg [reg]
+  (->>
+    (db/query '[:find (pull ?e [*])
+                :in $ ?ignored-tags ?reg
+                :where
+                [?e :doctor/type :type/garden]
+                ;; TODO parse file-tags to :org/tags in org-crud
+                [?e :org/level :level/root]
+                ;; [?e :org/tags ?tag]
+                ;; [(not (contains? ?ignored-tags ?tag))]
+
+                [?e :org/source-file ?src-file]
+                [(re-seq ?reg ?src-file)]]
+              ignored-tags reg)
+    (map first)
+    (sort-by :org/source-file)
+    (reverse)
+    ))
+
+(comment
+  (->>
+    (notes-matching-source-file-reg #"/daily/2022-0[6|7]")
+    (take 10)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; collecting for tags
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (def post-tags #{"til" "post" "posts" "blog"})
 
 (defn note->expo-post [{:keys [] :as note}]
   (-> note
       (assoc :expo/type :type/post)
       (assoc :expo/title (:org/name note))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; collecting for dailies
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 
 (defn update-posts []
   (->>
