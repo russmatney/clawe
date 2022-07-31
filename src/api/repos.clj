63e82898e11ea0370cb31da922ef5182da03330f
Russell Matney <russell.matney@gmail.com>
2022-07-30 23:10:19 -0400

    wip: repos sync

--- src/api/repos.clj
+++ src/api/repos.clj
@@ -2,7 +2,48 @@
   (:require
    [systemic.core :refer [defsys] :as sys]
    [manifold.stream :as s]
-   [clawe.git :as clawe.git]))
+   [clawe.git :as clawe.git]
+   [defthing.db :as db]
+   [babashka.fs :as fs]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; db
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn workspace-git-dirs
+  "Fetches git-dirs for all :workspace/directories in the db"
+  []
+  (->>
+    (db/query
+      '[:find ?directory
+        :where
+        ;; TODO what field should we pull?
+        [?e :workspace/directory ?directory]])
+    (map first)
+    (filter #(fs/exists? (str % "/.git")))))
+
+(defn dir->db-repo [dir]
+  ;; TODO validation
+  ;; TODO check for git-repo
+  ;; TODO attach current status
+  ;; TODO last-git-status timestamp
+  {:doctor/type    :type/repo
+   :repo/directory dir})
+
+(defn sync-repos-to-db []
+  (let [workspace-git-dirs (workspace-git-dirs)]
+    (->> workspace-git-dirs
+         (map dir->db-repo)
+         (db/transact))))
+
+(comment
+  (sync-repos-to-db)
+  )
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; api
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn ->repo [path]
   {:repo/path path})
