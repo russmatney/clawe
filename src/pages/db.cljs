e35b4291dd6dba6461e82835c4dd7d6f49d61e91
Russell Matney <russell.matney@gmail.com>
2022-08-07 20:52:56 -0400

    wip: db page queries

--- src/pages/db.cljs
+++ src/pages/db.cljs
@@ -1,7 +1,33 @@
 (ns pages.db
   (:require
    [hooks.db :as hooks.db]
-   [datascript.core :as d]))
+   [datascript.core :as d]
+   [components.debug :as components.debug]
+   [components.garden :as components.garden]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; db by :doctor/type
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn ents-with-doctor-type [conn]
+  (when conn
+    (->> (d/q '[:find (pull ?e [*])
+                :where
+                [?e :doctor/type _]]
+              conn)
+         (map first))))
+
+(comment
+  (declare conn)
+  (->
+    conn
+    ents-with-doctor-type
+    first
+    (dissoc :org/id)
+    (select-keys [:garden/file-name])
+    )
+  )
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; event page
@@ -9,14 +35,8 @@
 
 (defn page [_opts]
   (let [{:keys [conn]} (hooks.db/use-db)
-        data
-        (when conn
-          (println "conn" conn)
-          (some->> (d/q '[:find (pull ?e [*])
-                          :where ;; TODO create and use an expo type
-                          [?e _ _]]
-                        conn)
-                   (map first)))]
+        ents           (ents-with-doctor-type conn)]
+    (def conn conn)
     [:div
      {:class ["flex" "flex-col" "flex-auto"
               "min-h-screen"
@@ -29,12 +49,27 @@
 
      [:div
       {:class ["font-mono"]}
-      (pr-str conn)]
 
-     [:div
-      {:class ["font-mono"]}
-      (doall
-        (->> data
-             (map str)))]
+      (for [[i [k ents-by-type]]
+            (->> ents
+                 (group-by :doctor/type)
+                 (map-indexed vector))]
+        ^{:key i}
+        [:div
+         (when (zero? i)
+           (some-> k str))
 
-     ]))
+         (for [[i ent] (->> ents-by-type (map-indexed vector))]
+           (let []
+             (def ent ent)
+             ^{:key i}
+             [:div
+              [components.garden/garden-node ent]
+
+              #_[components.garden/org-file ent]
+
+              [components.debug/raw-metadata ent]]))])]
+
+     #_[:div
+        {:class ["font-mono"]}
+        (pr-str conn)]]))
