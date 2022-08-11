6b1e2c1bf66529b747a22781adbf52419a0c02ed
Russell Matney <russell.matney@gmail.com>
2022-08-11 14:50:40 -0400

    feat: allow updating doctor topbar on mac, from -x
    bb-cli can call any func! as long as it expects args.

--- src/clawe/doctor.clj
+++ src/clawe/doctor.clj
@@ -5,14 +5,13 @@
 (defn update-topbar-url []
   (str (clawe.config/doctor-base-url) "/topbar/update"))
 
-(defn update-topbar []
-  (when-not (clawe.config/is-mac?)
-    (slurp (update-topbar-url))))
+(defn update-topbar
+  ([] (update-topbar nil))
+  ([_] (slurp (update-topbar-url))))
 
 (defn reload []
   (when-not (clawe.config/is-mac?)
     (slurp (str (clawe.config/doctor-base-url) "/reload"))))
 
 (defn update-screenshots []
-  (when-not (clawe.config/is-mac?)
-    (slurp (str (clawe.config/doctor-base-url) "/screenshots/update"))))
+  (slurp (str (clawe.config/doctor-base-url) "/screenshots/update")))
