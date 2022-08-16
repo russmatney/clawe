473ab99791440c68dc2ac96e7f33487d089bb18a
Russell Matney <russell.matney@gmail.com>
2022-08-16 18:28:45 -0400

    feat: pull out components/filter
    Pulls the todos filtering logic into a reusable use-filter hook, which
    returns filtered items, filtered+grouped items, and a component for
    manipulating the filters themselves.
    
    This expects to be passed a set of filters and some defaults, which
    should be malli-speced to provide warnings/make this more consumable.

---
+++ src/components/filter.cljs
@@ -0,0 +1,160 @@
+(ns components.filter
+  (:require [components.floating :as floating]
+            [uix.core.alpha :as uix]))
+
+(defn- expand-coll-group-bys [items-grouped-by]
+  (reduce
+    (fn [item-groups-by-label [labels items]]
+      (if (coll? labels)
+        ;; update for each label
+        (reduce
+          #(update %1 %2 concat items)
+          item-groups-by-label
+          labels)
+        ;; simple case, just passing through
+        (assoc item-groups-by-label labels items)))
+    {}
+    items-grouped-by))
+
+(defn- filter-grouper
+  "A component for displaying and selecting filters/groups.
+
+  Returned as part of `use-filter`."
+  [items {:keys [all-filter-defs
+                 set-group-by toggle-filter-by
+                 items-group-by items-filter-by]}]
+  [:div.flex.flex-row.flex-wrap
+   {:class ["gap-x-3"]}
+
+   (for [[i [filter-key filter-def]] (map-indexed vector all-filter-defs)]
+     (let [grouped-by-counts
+           (->> items
+                (group-by (:group-by filter-def))
+                expand-coll-group-bys
+                (map (fn [[v xs]] [v (count xs)])))
+
+           group-by-enabled? (= items-group-by filter-key)]
+       [:div
+        {:key i}
+
+        [floating/popover
+         {:hover true :click true
+          :anchor-comp
+          [:div.text-xl.font-nes
+           {:class    ["cursor-pointer"
+                       "hover:text-city-red-600"
+                       (when group-by-enabled? "text-city-pink-400")]
+            :on-click #(set-group-by filter-key)}
+           (:label filter-def)]
+          :popover-comp
+          [:div
+           {:class ["bg-city-blue-800"
+                    "text-city-pink-200"
+                    "py-2" "px-4"]}
+
+           ;; custom filter-opt selection
+           (when (seq (:filter-options filter-def))
+             [:div
+              (for [[i filter-option] (->> filter-def :filter-options (map-indexed vector))]
+                (let [filter-enabled? (items-filter-by (assoc filter-option :filter-key filter-key))]
+                  [:div
+                   {:key      i
+                    :on-click #(toggle-filter-by (assoc filter-option :filter-key filter-key))
+                    :class    ["flex" "flex-row" "font-mono"
+                               "cursor-pointer"
+                               "hover:text-city-red-600"
+                               (when filter-enabled? "text-city-pink-400")]}
+                   (:label filter-option)]))])
+
+           (let [group-filters-by (:group-filters-by filter-def (fn [_] nil))]
+             [:div
+              {:class ["flex" "flex-row" "flex-wrap" "gap-x-4"]}
+              (for [[i [group-label group]] (->> grouped-by-counts
+                                                 (group-by (comp group-filters-by first))
+                                                 (sort-by first)
+                                                 (map-indexed vector))]
+                [:div
+                 {:key   i
+                  :class ["flex" "flex-col"]}
+
+                 (when group-label
+                   [:div
+                    {:class ["font-nes" "mx-auto"]}
+                    group-label])
+
+                 (for [[i [k v]] (->> group (sort-by first >) (map-indexed vector))]
+                   (let [filter-enabled? (items-filter-by {:filter-key filter-key :match k})]
+                     [:div
+                      {:key      i
+                       :class    ["flex" "flex-row" "font-mono"
+                                  "cursor-pointer"
+                                  "hover:text-city-red-600"
+                                  (when filter-enabled?
+                                    "text-city-pink-400")]
+                       :on-click #(toggle-filter-by {:filter-key filter-key :match k})}
+                      (let [format-label (:format-label filter-def str)]
+                        [:span.p-1.pl-2.text-xl.ml-auto (format-label k)])
+                      [:span.p-1.text-xl.w-10.text-center v]]))])])]}]]))])
+
+(defn- filter-match-fn
+  "Builds a predicate for an item, for a grouped filter-key.
+
+  Multiple filter-defs per key can be 'on' at a time
+  (i.e. filtering by more than one tag or source-file),
+  so here we build a predicate that returns true if any of the passed 'filter-defs' match.
+
+  The matches can be exact or predicates themselves.
+
+  The filter-key's group-by can return collections - in that case, a match on any elem
+  is a match for the whole filter-key."
+  [all-filter-defs [filter-key filter-defs]]
+  (let [->value       (-> filter-key all-filter-defs :group-by)
+        exact-matches (->> filter-defs (map :match) (into #{}))
+        pred-matches  (->> filter-defs (map :match-fn) (remove nil?))
+        is-match      (->> [exact-matches]
+                           (filter seq)
+                           (concat pred-matches)
+                           ((fn [fns]
+                              (apply some-fn fns))))]
+    (fn [raw]
+      (-> raw ->value
+          ((fn [val]
+             (if (coll? val)
+               (->> val (filter is-match) seq)
+               (is-match val))))))))
+
+(defn use-filter [{:keys [items default-filters default-group-by all-filter-defs]}]
+  ;; TODO malli schema+validation for all-filter-defs and default-filters
+  (let [items-group-by  (uix/state
+                          (or default-group-by
+                              (some->> all-filter-defs first first)))
+        items-filter-by (uix/state default-filters)
+
+        filtered-items
+        (if-not (seq @items-filter-by) items
+                (->> items (filter
+                             (apply every-pred
+                                    ;; every predicate must match
+                                    (->> @items-filter-by
+                                         (group-by :filter-key)
+                                         (map (partial
+                                                filter-match-fn all-filter-defs)))))))
+
+        filtered-item-groups
+        (->> filtered-items
+             (group-by (some-> @items-group-by all-filter-defs :group-by))
+             expand-coll-group-bys
+             (map (fn [[label its]] {:item-group its :label label})))]
+
+    {:filter-grouper
+     [filter-grouper items
+      {:all-filter-defs all-filter-defs
+       :set-group-by    #(reset! items-group-by %)
+       :toggle-filter-by
+       (fn [f-by]
+         (swap! items-filter-by
+                #(if (% f-by) (disj % f-by) (conj % f-by))))
+       :items-filter-by @items-filter-by
+       :items-group-by  @items-group-by}]
+     :filtered-items       filtered-items
+     :filtered-item-groups filtered-item-groups}))
--- src/pages/todos.cljs
+++ src/pages/todos.cljs
@@ -2,7 +2,7 @@
   (:require
    [uix.core.alpha :as uix]
    [components.todo :as components.todo]
-   [components.floating :as floating]
+   [components.filter :as components.filter]
    [clojure.string :as string]
    [tick.core :as t]
    [doctor.ui.db :as ui.db]
@@ -45,6 +45,8 @@
                                                  (t/format "MMM d, YYYY")))
                                         "Unscheduled"))}})
 
+(def default-group-by :short-path)
+
 (def default-filters
   #{{:filter-key :status :match :status/not-started}
     {:filter-key :status :match :status/in-progress}
@@ -57,142 +59,19 @@
      :match-fn   is-workspace-fname
      :label      "All Workspaces"}})
 
-(defn filter-grouper [items {:keys [set-group-by toggle-filter-by
-                                    items-group-by items-filter-by]}]
-  [:div.flex.flex-row.flex-wrap
-   {:class ["gap-x-3"]}
-   (for [[i [filter-key filter-def]] (map-indexed vector all-filter-defs)]
-     (let [split             (->> items
-                                  (group-by (:group-by filter-def))
-                                  (reduce (fn [item-groups-by-label [labels items]]
-                                            (if (coll? labels)
-                                              ;; update for each label
-                                              (reduce
-                                                (fn [by-label label]
-                                                  (update by-label label concat items))
-                                                item-groups-by-label
-                                                labels)
-                                              ;; simple case, just passing through
-                                              (assoc item-groups-by-label labels items)))
-                                          {})
-                                  (map (fn [[v xs]] [v (count xs)])))
-           group-by-enabled? (= items-group-by filter-key)]
-       [:div
-        {:key i}
-
-        [floating/popover
-         {:hover true :click true
-          :anchor-comp
-          [:div.text-xl.font-nes
-           {:class    ["cursor-pointer"
-                       "hover:text-city-red-600"
-                       (when group-by-enabled?
-                         "text-city-pink-400")]
-            :on-click #(set-group-by filter-key)}
-           (:label filter-def)]
-          :popover-comp
-          [:div
-           {:class ["bg-city-blue-800"
-                    "text-city-pink-200"
-                    "py-2" "px-4"]}
-
-           ;; custom filter-opt selection
-           (when (seq (:filter-options filter-def))
-             [:div
-              (for [[i filter-option] (->> filter-def :filter-options (map-indexed vector))]
-                (let [filter-enabled? (items-filter-by (assoc filter-option :filter-key filter-key))]
-                  [:div
-                   {:key      i
-                    :on-click #(toggle-filter-by (assoc filter-option :filter-key filter-key))
-                    :class    ["flex" "flex-row" "font-mono"
-                               "cursor-pointer"
-                               "hover:text-city-red-600"
-                               (when filter-enabled?
-                                 "text-city-pink-400")]}
-                   (:label filter-option)]))])
-
-           (let [group-filters-by (:group-filters-by filter-def (fn [_] nil))]
-             [:div
-              {:class ["flex" "flex-row" "flex-wrap" "gap-x-4"]}
-              (for [[i [group-label group]] (->> split
-                                                 (group-by (comp group-filters-by first))
-                                                 (sort-by first)
-                                                 (map-indexed vector))]
-                [:div
-                 {:key   i
-                  :class ["flex" "flex-col"]}
-
-                 (when group-label
-                   [:div
-                    {:class ["font-nes" "mx-auto"]}
-                    group-label])
-
-                 (for [[i [k v]] (->> group (sort-by first >) (map-indexed vector))]
-                   (let [filter-enabled? (items-filter-by {:filter-key filter-key :match k})]
-                     [:div
-                      {:key      i
-                       :class    ["flex" "flex-row" "font-mono"
-                                  "cursor-pointer"
-                                  "hover:text-city-red-600"
-                                  (when filter-enabled?
-                                    "text-city-pink-400")]
-                       :on-click #(toggle-filter-by {:filter-key filter-key :match k})}
-                      (let [format-label (:format-label filter-def str)]
-                        [:span.p-1.pl-2.text-xl.ml-auto (format-label k)])
-                      [:span.p-1.text-xl.w-10.text-center v]]))])])]}]]))])
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; page
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn page [{:keys [conn]}]
-  (let [todos (ui.db/garden-todos conn {:n 1000})
-
+  (let [todos    (ui.db/garden-todos conn {:n 1000})
         selected (uix/state (first todos))
-
-        ;; TODO refactor this filtering/sorting stuff into a component/hook
-        items-group-by  (uix/state (some->> all-filter-defs first first))
-        items-filter-by (uix/state default-filters)
-
-        filtered-items
-        (if-not (seq @items-filter-by) todos
-                (let [preds
-                      (->> @items-filter-by
-                           (group-by :filter-key)
-                           (map (fn [[filter-key filter-defs]]
-                                  (let [->value       (-> filter-key all-filter-defs :group-by)
-                                        exact-matches (->> filter-defs (map :match) (into #{}))
-                                        pred-matches  (->> filter-defs (map :match-fn) (remove nil?))
-                                        is-match      (->> [exact-matches]
-                                                           (filter seq)
-                                                           (concat pred-matches)
-                                                           ((fn [fns]
-                                                              (apply some-fn fns))))]
-                                    (fn [raw]
-                                      (-> raw ->value
-                                          ((fn [val]
-                                             (println "is-match?" val)
-                                             (if (coll? val)
-                                               (->> val (filter is-match) seq)
-                                               (is-match val))))))))))]
-                  (->> todos (filter (apply every-pred preds)))))
-
-        filtered-item-groups (->> filtered-items
-                                  (group-by (some-> @items-group-by all-filter-defs :group-by))
-                                  (reduce (fn [item-groups-by-label [labels items]]
-                                            (if (coll? labels)
-                                              ;; update for each label
-                                              (reduce
-                                                (fn [by-label label]
-                                                  (update by-label label concat items))
-                                                item-groups-by-label
-                                                labels)
-                                              ;; simple case, just passing through
-                                              (assoc item-groups-by-label labels items)))
-                                          {})
-                                  (map (fn [[label its]]
-                                         {:item-group its
-                                          :label      label})))]
+        {:keys [filtered-item-groups filtered-items filter-grouper]}
+        (components.filter/use-filter
+          {:all-filter-defs  all-filter-defs
+           :default-filters  default-filters
+           :default-group-by default-group-by
+           :items            todos})]
 
     [:div
      {:class ["grid" "grid-flow-row" "place-items-center"
@@ -200,15 +79,7 @@
               "min-h-screen"
               "text-city-pink-200"]}
 
-     [:div
-      {:class ["p-4"]}
-      [filter-grouper todos {:set-group-by    #(reset! items-group-by %)
-                             :toggle-filter-by
-                             (fn [f-by]
-                               (swap! items-filter-by
-                                      #(if (% f-by) (disj % f-by) (conj % f-by))))
-                             :items-filter-by @items-filter-by
-                             :items-group-by  @items-group-by}]]
+     [:div {:class ["p-4"]} filter-grouper]
 
      [:div
       {:class ["grid" "grid-flow-row"]}
@@ -217,22 +88,17 @@
        {:label     "In Progress"
         :on-select (fn [it] (reset! selected it))
         :selected  @selected}
-       (->> todos (filter
-                    (fn [todo]
-                      (or
-                        (-> todo :org/status #{:status/in-progress})
-                        (and
-                          (-> todo :todo/queued-at)
-                          (not
-                            (-> todo :org/status #{:status/cancelled
-                                                   :status/done})))))))]
-
-      #_[components.todo/todo-list
-         {:label     "All Incomplete"
-          :on-select (fn [it] (reset! selected it))
-          :selected  @selected}
-         (->> todos (remove (comp #{:status/done
-                                    :status/cancelled} :org/status)))]
+       (->> #_todos
+            filtered-items
+            (filter
+              (fn [todo]
+                (or
+                  (-> todo :org/status #{:status/in-progress})
+                  (and
+                    (-> todo :todo/queued-at)
+                    (not
+                      (-> todo :org/status #{:status/cancelled
+                                             :status/done})))))))]
 
       (for [[i {:keys [item-group label]}]
             (->> filtered-item-groups
@@ -242,5 +108,5 @@
         [components.todo/todo-list {:label     (str label " (" (count item-group) ")")
                                     :on-select (fn [it] (reset! selected it))
                                     :selected  @selected
-                                    :n         6}
+                                    :n         9}
          item-group])]]))
