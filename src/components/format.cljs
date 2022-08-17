8ec5c0bf7ddc8b1ce20ce4e3501195adec99e3de
Russell Matney <russell.matney@gmail.com>
2022-08-17 16:00:14 -0400

    refactor: clean up posts page layout

--- src/components/format.cljs
+++ src/components/format.cljs
@@ -1,4 +1,6 @@
-(ns components.format)
+(ns components.format
+  (:require
+   [tick.core :as t]))
 
 (defn s-shortener
   ([s] (s-shortener nil s))
@@ -17,3 +19,14 @@
     {:length 20}
     "some really long string with lots of thoughts that never end")
   )
+
+(defn last-modified-human [last-modified]
+  (let [time-ago  (t/duration {:tick/beginning (t/instant last-modified)
+                               :tick/end       (t/now)})
+        mins-ago  (t/minutes time-ago)
+        hours-ago (t/hours time-ago)
+        days-ago  (t/days time-ago)]
+    (cond
+      (< mins-ago 60)  (str mins-ago " min(s) ago")
+      (< hours-ago 24) (str hours-ago " hour(s) ago")
+      :else            (str days-ago " day(s) ago"))))
--- src/pages/posts.cljs
+++ src/pages/posts.cljs
@@ -4,59 +4,35 @@
    [wing.uix.router :as router]
    [uix.core.alpha :as uix]
    [clojure.string :as string]
-   [tick.core :as t]
 
    [components.garden :as components.garden]
-   [components.floating :as components.floating]
    [components.format :as components.format]
    [doctor.ui.db :as ui.db]))
 
 (defn post-link
-  "A link to a blog-post-y rendering of a garden note"
+  "A clickable name, tags, and last-modified at"
   [{:keys [on-select is-selected?]} item]
-  (let [{:org/keys      [tags]
-         :org.prop/keys [title]
-         :time/keys     [last-modified]} item
-        hovering?                        (uix/state false)]
+  (let [{:org.prop/keys [title]
+         :file/keys     [last-modified]} item]
     [:div
-     {:class (->> ["flex" "flex-row" "flex-wrap"
-                   "px-2"
-                   "align-items"
-                   (when is-selected? "border border-city-blue-600")
-                   "bg-yo-blue-700"
-                   "text-white"
-                   (when @hovering? "cursor-pointer")
-                   (when @hovering? "hover:text-city-blue-400")])
-
-      :on-click       #(on-select)
-      :on-mouse-enter #(reset! hovering? true)
-      :on-mouse-leave #(reset! hovering? false)}
+     {:class    ["grid" "grid-flow-col"
+                 (when is-selected? "border border-city-blue-600")
+                 "bg-yo-blue-700"
+                 "text-white"
+                 "cursor-pointer" "hover:text-city-blue-400"]
+      :on-click #(on-select)}
 
      [:span
       {:class ["px-2" "font-mono"]}
       (components.format/s-shortener {:length 18} title)]
 
-     ;; filename
-     #_[:div
-        {:class ["font-mono"]}
-        (components.format/s-shortener source-file)]
-
      [components.garden/tags-comp item]
 
-     [:span.ml-auto
-      (when last-modified
-        [:div
-         {:class ["font-mono"
-                  "text-gray-500"]}
-         (let [time-ago  (t/duration {:tick/beginning (t/instant last-modified)
-                                      :tick/end       (t/now)})
-               mins-ago  (t/minutes time-ago)
-               hours-ago (t/hours time-ago)
-               days-ago  (t/days time-ago)]
-           (cond
-             (< mins-ago 60)  (str mins-ago " min(s) ago")
-             (< hours-ago 24) (str hours-ago " hour(s) ago")
-             :else            (str days-ago " day(s) ago")))])]]))
+     (when last-modified
+       [:div
+        {:class ["justify-self-end"
+                 "font-mono" "text-gray-500"]}
+        (components.format/last-modified-human last-modified)])]))
 
 (defn page [{:keys [conn]}]
   (let [selected-item-name (router/use-route-parameters [:query :item-name])
@@ -75,35 +51,21 @@
         ;; TODO preserve selection (query params?)
         last-selected (uix/state default-selection)
         open-posts    (uix/state #{default-selection})]
-    (println "item-name" @selected-item-name)
-    ;; Posts
-    ;; List of post names grouped by tag
-    ;; Maybe groups of linked nodes
-    ;; use d3? use godot?
-
     [:div
-     {:class ["bg-yo-blue-500"
-              "flex" "flex-col" "flex-wrap"
-              "min-h-screen"
-              "overflow-y-auto"]}
+     {:class ["grid" "grid-flow-row"
+              "bg-yo-blue-500"]}
 
-     ;; list/selected
      [:div
-      {:class ["flex" "flex-row"]}
+      {:class ["grid" "grid-flow-col"]}
 
+      ;; list of note titles
       [:div
-       {:class ["flex"
-                "flex-grow-0"
-                "flex-col" "flex-wrap"
-                "p-2"
-                "justify-center"
+       {:class ["grid" "grid-flow-row" "p-2"
                 "bg-yo-blue-700"
-                "max-w-100"]}
+                "w-100"]}
        (for [[i it] (->> items
-                         ;; TODO some fancy grouping/sorting/filtering feats
                          (sort-by :file/last-modified >)
                          (map-indexed vector))]
-
          ^{:key i}
          [post-link
           {:on-select    (fn [_]
@@ -114,11 +76,10 @@
            :is-selected? (@open-posts it)}
           (assoc it :index i)])]
 
+      ;; selected notes
       (when (seq @open-posts)
         [:div
-         {:class ["flex"
-                  "flex-grow-1"
-                  "p-2"
+         {:class ["grid" "p-2"
                   "bg-yo-blue-700"]}
          (for [[i p] (->> @open-posts
                           seq
