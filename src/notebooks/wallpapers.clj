d48b2b521847ef1b04f8250a5dd8eb469289d227
Russell Matney <russell.matney@gmail.com>
2022-09-25 15:46:49 -0500

    wip: clerk wallpaper setting demo
    Begins this feat, but blocked by no wifi

---
+++ src/notebooks/wallpapers.clj
@@ -0,0 +1,14 @@
+^{:nextjournal.clerk/visibility {:code :hide}}
+(ns notebooks.wallpapers
+  (:require
+   [wallpapers.core :as wallpapers]
+   [nextjournal.clerk :as clerk]))
+
+;; # wallpapers
+
+#_(clerk/table
+    (->>
+      (wallpapers/all-wallpapers)
+      (map (fn [{:keys [file/web-asset-path file/file-name]}]
+             {:path web-asset-path
+              :file file-name}))))
