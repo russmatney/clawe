73556add1719c00697d570645dce903abc335905
Russell Matney <russell.matney@gmail.com>
2022-09-30 14:40:21 -0400

    feat: clerk clawe notebook clean up
    Using ns-level flags is much cleaner.

--- src/notebooks/clawe.clj
+++ src/notebooks/clawe.clj
@@ -1,6 +1,7 @@
-^{:nextjournal.clerk/visibility {:code :hide}}
 (ns notebooks.clawe
-  {:nextjournal.clerk/toc true}
+  {:nextjournal.clerk/toc        true
+   :nextjournal.clerk/visibility {:code :hide}
+   :nextjournal.clerk/no-cache   true}
   (:require
    [clawe.debug :as debug]
    [nextjournal.clerk :as clerk]
@@ -8,47 +9,38 @@
    [clawe.wm :as wm]))
 
 ;; ## current workspace
-^{::clerk/visibility {:code :hide}
-  ::clerk/no-cache   true}
 (->>
   (wm/current-workspace)
   (remove (comp #(if (coll? %) (not (seq %)) (nil? %)) second))
   (into {}))
 
 ;; ### clients
-^{::clerk/visibility {:code :hide}
-  ::clerk/no-cache   true}
+
 (clerk/table
   {::clerk/width :full}
   (debug/ls {:type :clients}))
 
 ;; ### clients-all
-^{::clerk/visibility {:code :hide}
-  ::clerk/no-cache   true}
+
 (clerk/table
   {::clerk/width :full}
   (debug/ls {:type :clients
              :all  true}))
 
 ;; ### workspaces
-^{::clerk/visibility {:code :hide}
-  ::clerk/no-cache   true}
+
 (clerk/table
   {::clerk/width :full}
   (debug/ls {:type :workspaces}))
 
 ;; ### tmux sessions
 
-^{::clerk/visibility {:code :hide}
-  ::clerk/no-cache   true}
 (clerk/table
   {::clerk/width :full}
   (debug/ls {:type :tmux}))
 
 ;; ### tmux panes
 
-^{::clerk/visibility {:code :hide}
-  ::clerk/no-cache   true}
 (clerk/table
   {::clerk/width :full}
   (debug/ls {:type :tmux-panes}))
@@ -56,15 +48,13 @@
 ;; # defs
 
 ;; ### client-defs
-^{::clerk/visibility {:code :hide}
-  ::clerk/no-cache   true}
+
 (clerk/table
   {::clerk/width :full}
   (clawe.config/client-defs))
 
 ;; ### workspace-defs
-^{::clerk/visibility {:code :hide}
-  ::clerk/no-cache   true}
+
 (clerk/table
   {::clerk/width :full}
   (vals (clawe.config/workspace-defs-with-titles)))
