10703c1cb77a0a8e6b8fd5784446eb90f1f31c77
Russell Matney <russell.matney@gmail.com>
2022-09-30 15:16:23 -0400

    feat: initial git-status clerk notebook

---
+++ src/notebooks/git_status.clj
@@ -0,0 +1,38 @@
+(ns notebooks.git-status
+  {:nextjournal.clerk/toc        true
+   :nextjournal.clerk/visibility {:code :hide}}
+  (:require
+   [ralphie.git :as ralphie.git]
+   [nextjournal.clerk :as clerk]
+   [clawe.config :as clawe.config]
+   [babashka.fs :as fs]
+   [ralphie.zsh :as zsh]))
+
+;; NOTE fetching is not implemented yet, so :needs-pull? will never be set
+
+^{::clerk/visibility {:result :hide}
+  ::clerk/no-cache   true}
+(def clawe-workspace-repos
+  (->>
+    (clawe.config/workspace-defs-with-titles)
+    vals
+    (filter :workspace/directory)
+    (filter (comp fs/exists? #(str % "/.git") zsh/expand :workspace/directory))
+    (map (fn [{:workspace/keys [directory] :as x}]
+           (merge x (ralphie.git/status directory))))))
+
+;; ## dirty
+
+(clerk/table
+  {::clerk/width :full}
+  (->>
+    clawe-workspace-repos
+    (filter (fn [{:git/keys [dirty? needs-pull? needs-push?]}]
+              (or dirty? needs-pull? needs-push?)))))
+
+
+;; ## all repos
+
+(clerk/table
+  {::clerk/width :full}
+  clawe-workspace-repos)
