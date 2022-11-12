bdbb01bf01c76c39959348f30234c977c7f198b6
Russell Matney <russell.matney@gmail.com>
2022-11-12 15:13:25 -0500

    feat: fix bg height, misc todo header styling

--- src/doctor/ui/views/focus.cljs
+++ src/doctor/ui/views/focus.cljs
@@ -1,7 +1,8 @@
 (ns doctor.ui.views.focus
   (:require
    [doctor.ui.hooks.use-focus :as use-focus]
-   [clojure.set :as set]))
+   [clojure.set :as set]
+   [clojure.string :as string]))
 
 (defn completed? [it]
   (-> it :org/status #{:status/done}))
@@ -13,34 +14,53 @@
   (-> it :org/status #{:status/not-started}))
 
 (defn item-name [it]
-  [:span
-   [:span
-    {:class ["pr-4"]
-     :style {:padding-left
-             (let [level (:org/level it 0)
-                   level (if (#{:level/root} level) 0 level)]
-               (str (* 12 level) "px"))}}
-    (cond
-      (completed? it)   "[X]"
-      (current? it)     "[-]"
-      (not-started? it) "[ ]")]
-   (:org/name it)])
+  (let [level (:org/level it 0)
+        level (if (#{:level/root} level) 0 level)]
+    [:span
+     {:class ["flex" "flex-row"]}
+     [:span (->> (repeat level "*") (apply str))]
+     [:span
+      {:class ["px-4" "whitespace-nowrap"]}
+      (cond
+        (completed? it)   "[X]"
+        (current? it)     "[-]"
+        (not-started? it) "[ ]")]
+     [:span
+      {:class
+       (concat (cond
+                 (completed? it)   ["line-through"]
+                 (current? it)     ["font-bold"
+                                    "font-nes"]
+                 (not-started? it) []
+                 :else             ["font-normal"])
+               ["whitespace-nowrap"])}
+      (:org/name it)]
+     (when (seq (:org/tags it))
+       [:span
+        {:class (concat (cond
+                          (completed? it)   ["line-through"]
+                          (current? it)     ["font-bold"]
+                          (not-started? it) []
+                          :else             ["font-normal"])
+                        ["pl-4"])}
+        (->> (:org/tags it)
+             (string/join ":")
+             (#(str ":" % ":")))])]))
 
 (defn item-header [it]
   [:h1 {:class
         (concat
-          ["text-3xl"
-           "font-mono"
-           "py-4"]
+          ["text-4xl"
+           "py-4"
+           "font-mono"]
           (cond
             (completed? it)   ["text-slate-800"
                                "line-through"]
             (current? it)     ["text-city-green-400"
-                               "text-weight-600"
-                               "text-4xl"
-                               ]
+                               "font-bold"]
             (not-started? it) ["text-city-pink-300"]
-            :else             ["text-yo-blue-200"]))}
+            :else             ["text-yo-blue-200"
+                               "font-normal"]))}
    (item-name it)])
 
 (defn widget [_opts]
@@ -48,15 +68,11 @@
         {:keys [todos]} @focus-data]
     [:div
      {:class ["bg-city-blue-800"
-              "bg-opacity-60"
-              "h-screen"]}
-     [:div
-      {:class ["bg-city-blue-800"
-               "bg-opacity-60"
-               "h-full"
-               "flex" "flex-col"]}
-      [:div {:class ["px-4"]}
+              "bg-opacity-90"
+              "min-h-screen"
+              "flex" "flex-col"]}
+     [:div {:class ["px-4"]}
 
-       (for [[i it] (->> todos (map-indexed vector))]
-         ^{:key i}
-         [item-header it])]]]))
+      (for [[i it] (->> todos (map-indexed vector))]
+        ^{:key i}
+        [item-header it])]]))
