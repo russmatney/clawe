12ce6ef45856aa23ccf2b1b36c33dc33d924cb71
Russell Matney <russell.matney@gmail.com>
2022-11-22 14:14:55 -0500

    fix: refactor and fix broken cljs timeline tests

--- test/components/timeline_test.cljs
+++ test/components/timeline_test.cljs
@@ -8,56 +8,6 @@
    [tick.locale-en-us]
    [tick.core :as t]))
 
-(deftest timestamps-date-data-test
-  (testing "a list of timestamps are expanded to include :newest, :oldest, months, etc"
-    (is (= (sut/timestamps-date-data
-             [#time/zoned-date-time "2022-02-26T15:47:52-05:00"
-              #time/zoned-date-time "2022-04-06T15:47:52-05:00"
-              #time/zoned-date-time "2021-12-26T15:47:52-05:00"])
-
-           {:timestamps
-            [#time/zoned-date-time "2022-02-26T15:47:52-05:00"
-             #time/zoned-date-time "2022-04-06T15:47:52-05:00"
-             #time/zoned-date-time "2021-12-26T15:47:52-05:00"]
-            :newest
-            #time/zoned-date-time "2022-04-06T15:47:52-05:00"
-            :oldest
-            #time/zoned-date-time "2022-03-24T15:47:52-05:00"
-            :all-dates
-            '(#time/zoned-date-time "2022-03-24T15:47:52-05:00"
-               #time/zoned-date-time "2022-03-25T15:47:52-05:00"
-               #time/zoned-date-time "2022-03-26T15:47:52-05:00"
-               #time/zoned-date-time "2022-03-27T15:47:52-05:00"
-               #time/zoned-date-time "2022-03-28T15:47:52-05:00"
-               #time/zoned-date-time "2022-03-29T15:47:52-05:00"
-               #time/zoned-date-time "2022-03-30T15:47:52-05:00"
-               #time/zoned-date-time "2022-03-31T15:47:52-05:00"
-               #time/zoned-date-time "2022-04-01T15:47:52-05:00"
-               #time/zoned-date-time "2022-04-02T15:47:52-05:00"
-               #time/zoned-date-time "2022-04-03T15:47:52-05:00"
-               #time/zoned-date-time "2022-04-04T15:47:52-05:00"
-               #time/zoned-date-time "2022-04-05T15:47:52-05:00"
-               #time/zoned-date-time "2022-04-06T15:47:52-05:00")
-            :all-months
-            (#time/zoned-date-time "2022-03-24T15:47:52-05:00")
-            :dates-by-month
-            {#time/month "MARCH"
-             [#time/zoned-date-time "2022-03-24T15:47:52-05:00"
-              #time/zoned-date-time "2022-03-25T15:47:52-05:00"
-              #time/zoned-date-time "2022-03-26T15:47:52-05:00"
-              #time/zoned-date-time "2022-03-27T15:47:52-05:00"
-              #time/zoned-date-time "2022-03-28T15:47:52-05:00"
-              #time/zoned-date-time "2022-03-29T15:47:52-05:00"
-              #time/zoned-date-time "2022-03-30T15:47:52-05:00"
-              #time/zoned-date-time "2022-03-31T15:47:52-05:00"]
-             #time/month "APRIL"
-             [#time/zoned-date-time "2022-04-01T15:47:52-05:00"
-              #time/zoned-date-time "2022-04-02T15:47:52-05:00"
-              #time/zoned-date-time "2022-04-03T15:47:52-05:00"
-              #time/zoned-date-time "2022-04-04T15:47:52-05:00"
-              #time/zoned-date-time "2022-04-05T15:47:52-05:00"
-              #time/zoned-date-time "2022-04-06T15:47:52-05:00"]}}))))
-
 (comment
   (def --dates
     [(t/zoned-date-time)
@@ -81,3 +31,63 @@
        (map t/zoned-date-time)
        (map (fn [date-month]
               (t/format (t/formatter "MMM") date-month)))))
+
+(defn ->zoned-date-time [s]
+  (t/parse-zoned-date-time s (t/formatter :iso-zoned-date-time)))
+
+(deftest timestamps-date-data-test
+  (let [res (sut/timestamps-date-data
+              [(->zoned-date-time "2022-02-26T15:47:52-05:00")
+               (->zoned-date-time "2022-04-06T15:47:52-05:00")
+               (->zoned-date-time "2021-12-26T15:47:52-05:00")])]
+    (testing ":timestamps"
+      (is (= (:timestamps res)
+             [(->zoned-date-time "2022-02-26T15:47:52-05:00")
+              (->zoned-date-time "2022-04-06T15:47:52-05:00")
+              (->zoned-date-time "2021-12-26T15:47:52-05:00")])))
+
+    (testing ":newest, :oldest"
+      (is (= (:newest res)
+             (->zoned-date-time "2022-04-06T15:47:52-05:00")))
+      (is (= (:oldest res)
+             (->zoned-date-time "2022-03-24T15:47:52-05:00"))))
+
+    (testing ":all-dates"
+      (is (= (:all-dates res)
+             (list (->zoned-date-time "2022-03-24T15:47:52-05:00")
+                   (->zoned-date-time "2022-03-25T15:47:52-05:00")
+                   (->zoned-date-time "2022-03-26T15:47:52-05:00")
+                   (->zoned-date-time "2022-03-27T15:47:52-05:00")
+                   (->zoned-date-time "2022-03-28T15:47:52-05:00")
+                   (->zoned-date-time "2022-03-29T15:47:52-05:00")
+                   (->zoned-date-time "2022-03-30T15:47:52-05:00")
+                   (->zoned-date-time "2022-03-31T15:47:52-05:00")
+                   (->zoned-date-time "2022-04-01T15:47:52-05:00")
+                   (->zoned-date-time "2022-04-02T15:47:52-05:00")
+                   (->zoned-date-time "2022-04-03T15:47:52-05:00")
+                   (->zoned-date-time "2022-04-04T15:47:52-05:00")
+                   (->zoned-date-time "2022-04-05T15:47:52-05:00")
+                   (->zoned-date-time "2022-04-06T15:47:52-05:00")))))
+
+    (testing ":all-months"
+      (is (= (:all-months res)
+             (list (->zoned-date-time "2022-03-24T15:47:52-05:00")))))
+
+    (testing ":dates-by-month"
+      (is (= (:dates-by-month res)
+             {#time/month "MARCH"
+              [(->zoned-date-time "2022-03-24T15:47:52-05:00")
+               (->zoned-date-time "2022-03-25T15:47:52-05:00")
+               (->zoned-date-time "2022-03-26T15:47:52-05:00")
+               (->zoned-date-time "2022-03-27T15:47:52-05:00")
+               (->zoned-date-time "2022-03-28T15:47:52-05:00")
+               (->zoned-date-time "2022-03-29T15:47:52-05:00")
+               (->zoned-date-time "2022-03-30T15:47:52-05:00")
+               (->zoned-date-time "2022-03-31T15:47:52-05:00")]
+              #time/month "APRIL"
+              [(->zoned-date-time "2022-04-01T15:47:52-05:00")
+               (->zoned-date-time "2022-04-02T15:47:52-05:00")
+               (->zoned-date-time "2022-04-03T15:47:52-05:00")
+               (->zoned-date-time "2022-04-04T15:47:52-05:00")
+               (->zoned-date-time "2022-04-05T15:47:52-05:00")
+               (->zoned-date-time "2022-04-06T15:47:52-05:00")]})))))
