abf58fa41ec0449284c5c0ffd3340a156638269a
Russell Matney <russell.matney@gmail.com>
2022-11-21 21:55:09 -0500

    fix: don't use zsh before running tests

--- test_runner.clj
+++ test_runner.clj
@@ -2,26 +2,26 @@
 ;; https://book.babashka.org/#_running_tests
 
 (require '[clojure.test :as t]
-         '[babashka.process :as p]
+         ;; '[babashka.process :as p]
          '[babashka.classpath :as cp])
 
 (cp/add-classpath "src:test")
 (cp/add-classpath "../../teknql/wing/src")
 
-(def is-mac?
-  (let [ostype
-        (-> ^{:out :string}
-            (p/$ "zsh -c 'echo -n $OSTYPE'")
-            p/check :out)]
-    (re-seq #"^darwin" ostype)))
+;; (def is-mac?
+;;   (let [ostype
+;;         (-> ^{:out :string}
+;;             (p/$ "zsh -c 'echo -n $OSTYPE'")
+;;             p/check :out)]
+;;     (re-seq #"^darwin" ostype)))
 
 ;; TODO collect these automagically
 (def test-nses
   (->>
     (concat
-      (when-not is-mac?
-        ['ralphie.awesome-test
-         'ralphie.awesome.fnl-test])
+      ;; (when-not is-mac?
+      ;;   ['ralphie.awesome-test
+      ;;    'ralphie.awesome.fnl-test])
       ['defthing.core-test
        'defthing.defcom-test
        ;; 'db.core-test
