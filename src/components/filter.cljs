a94799f9abdadba634968870c70a923cad26b5ba
Russell Matney <russell.matney@gmail.com>
2023-01-31 15:49:59 -0500

    refactor: clean up filter-grouper component break down
    Breaks the filter-grouper into smaller pieces to hopefully make it
    easier to work with.

--- src/components/filter.cljs
+++ src/components/filter.cljs
@@ -4,96 +4,113 @@
             [components.debug :as components.debug]
             [util :as util]))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; filter def anchor
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn filter-def-anchor
+  [[filter-key filter-def] {:keys [set-group-by items-group-by]}]
+  (let [group-by-enabled? (= items-group-by filter-key)]
+    [:div.text-xl.font-nes
+     {:class    ["cursor-pointer"
+                 "hover:text-city-red-600"
+                 (when group-by-enabled? "text-city-pink-400")]
+      :on-click #(set-group-by filter-key)}
+     (:label filter-def)]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; filter def popover
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn filter-def-popover
+  [items [filter-key filter-def]
+   {:keys [items-filter-by toggle-filter-by]}]
+  (let [grouped-by-counts
+        (->> items
+             (group-by (:group-by filter-def))
+             util/expand-coll-group-bys
+             (map (fn [[v xs]] [v (count xs)])))]
+    [:div
+     {:class ["bg-city-blue-800"
+              "text-city-pink-200"
+              "py-2" "px-4"]}
+
+     ;; custom filter-opt selection
+     (when (seq (:filter-options filter-def))
+       [:div
+        (for [[i filter-option] (->> filter-def :filter-options (map-indexed vector))]
+          (let [filter-enabled? (items-filter-by (assoc filter-option :filter-key filter-key))]
+            [:div
+             {:key      i
+              :on-click #(toggle-filter-by (assoc filter-option :filter-key filter-key))
+              :class    ["flex" "flex-row" "font-mono"
+                         "cursor-pointer"
+                         "hover:text-city-red-600"
+                         (when filter-enabled? "text-city-pink-400")]}
+             (:label filter-option)]))])
+
+     (let [group-filters-by (:group-filters-by filter-def (fn [_] nil))]
+       [:div
+        {:class ["flex" "flex-row" "flex-wrap" "gap-x-4"]}
+        (for [[i [group-label group]] (->> grouped-by-counts
+                                           (group-by (comp group-filters-by first))
+                                           (sort-by first)
+                                           (map-indexed vector))]
+          [:div
+           {:key   i
+            :class ["flex" "flex-col"]}
+
+           (when group-label
+             [:div
+              {:class ["font-nes" "mx-auto"]}
+              group-label])
+
+           (for [[i [k v]] (->> group (sort-by first >) (map-indexed vector))]
+             (let [filter-enabled? (items-filter-by {:filter-key filter-key :match k})]
+               [:div
+                {:key      i
+                 :class    ["flex" "flex-row" "font-mono"
+                            "cursor-pointer"
+                            "hover:text-city-red-600"
+                            (when filter-enabled?
+                              "text-city-pink-400")]
+                 :on-click #(toggle-filter-by {:filter-key filter-key :match k})}
+                (let [format-label (:format-label filter-def str)]
+                  [:span.p-1.pl-2.text-xl.ml-auto (format-label k)])
+                [:span.p-1.text-xl.w-10.text-center v]]))])])]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; filter-grouper full component
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; let's not forget this wants to be a filter-grouper-sorter
 (defn- filter-grouper
   "A component for displaying and selecting filters/groups.
 
   Returned as part of `use-filter`."
-  [items {:keys [all-filter-defs
-                 set-group-by toggle-filter-by
-                 items-group-by items-filter-by]}]
-  [:div.flex.flex-row.flex-wrap
-   {:class ["gap-x-3"]}
+  [items {:keys [all-filter-defs items-filter-by]
+          :as   filter-grouper-config}]
+  [:div.flex.flex-col
+   [:div.flex.flex-row.flex-wrap
+    {:class ["gap-x-3"]}
 
-   ;; TODO break this up more
-   (for [[i [filter-key filter-def]] (map-indexed vector all-filter-defs)]
-     (let [grouped-by-counts
-           (->> items
-                (group-by (:group-by filter-def))
-                util/expand-coll-group-bys
-                (map (fn [[v xs]] [v (count xs)])))
-
-           group-by-enabled? (= items-group-by filter-key)]
-       [:div
-        {:key i}
-
-        [floating/popover
-         {:hover true :click true
-          :anchor-comp
-          [:div.text-xl.font-nes
-           {:class    ["cursor-pointer"
-                       "hover:text-city-red-600"
-                       (when group-by-enabled? "text-city-pink-400")]
-            :on-click #(set-group-by filter-key)}
-           (:label filter-def)]
-          :popover-comp
-          [:div
-           {:class ["bg-city-blue-800"
-                    "text-city-pink-200"
-                    "py-2" "px-4"]}
-
-           ;; custom filter-opt selection
-           (when (seq (:filter-options filter-def))
-             [:div
-              (for [[i filter-option] (->> filter-def :filter-options (map-indexed vector))]
-                (let [filter-enabled? (items-filter-by (assoc filter-option :filter-key filter-key))]
-                  [:div
-                   {:key      i
-                    :on-click #(toggle-filter-by (assoc filter-option :filter-key filter-key))
-                    :class    ["flex" "flex-row" "font-mono"
-                               "cursor-pointer"
-                               "hover:text-city-red-600"
-                               (when filter-enabled? "text-city-pink-400")]}
-                   (:label filter-option)]))])
-
-           (let [group-filters-by (:group-filters-by filter-def (fn [_] nil))]
-             [:div
-              {:class ["flex" "flex-row" "flex-wrap" "gap-x-4"]}
-              (for [[i [group-label group]] (->> grouped-by-counts
-                                                 (group-by (comp group-filters-by first))
-                                                 (sort-by first)
-                                                 (map-indexed vector))]
-                [:div
-                 {:key   i
-                  :class ["flex" "flex-col"]}
-
-                 (when group-label
-                   [:div
-                    {:class ["font-nes" "mx-auto"]}
-                    group-label])
-
-                 (for [[i [k v]] (->> group (sort-by first >) (map-indexed vector))]
-                   (let [filter-enabled? (items-filter-by {:filter-key filter-key :match k})]
-                     [:div
-                      {:key      i
-                       :class    ["flex" "flex-row" "font-mono"
-                                  "cursor-pointer"
-                                  "hover:text-city-red-600"
-                                  (when filter-enabled?
-                                    "text-city-pink-400")]
-                       :on-click #(toggle-filter-by {:filter-key filter-key :match k})}
-                      (let [format-label (:format-label filter-def str)]
-                        [:span.p-1.pl-2.text-xl.ml-auto (format-label k)])
-                      [:span.p-1.text-xl.w-10.text-center v]]))])])]}]]))
+    (for [[i [filter-key filter-def]] (map-indexed vector all-filter-defs)]
+      ^{:key i}
+      [floating/popover
+       {:hover        true :click true
+        :anchor-comp  [filter-def-anchor [filter-key filter-def] filter-grouper-config]
+        :popover-comp [filter-def-popover items [filter-key filter-def] filter-grouper-config]}])]
 
    ;; TODO improve debug view for collections
-   [components.debug/raw-metadata {:label "filters"} items-filter-by]
+   [components.debug/raw-metadata {:label "[raw filters]"} items-filter-by]
 
    ;; TODO create applied filters component for saving/revisiting saved filters
-   (for [[i f] (->> items-filter-by
-                    (map-indexed vector))]
-     ^{:key i}
-     [:div [:pre (str f)]])])
+   (for [[i f] (->> items-filter-by (map-indexed vector))]
+     ^{:key i} [:div [:pre (str f)]])])
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; filtering logic
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn- filter-match-fn
   "Builds a predicate for an item, for a grouped filter-key.
@@ -122,6 +139,10 @@
              (if (coll? val)
                (->> val (filter is-match) seq)
                (is-match val))))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; use-filter
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; TODO write a set of unit tests around this
 (defn use-filter [{:keys [items default-filters default-group-by all-filter-defs]}]
