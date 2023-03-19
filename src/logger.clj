c87aec024bcf1b02b819bcabb5ff11c2460b8728
Russell Matney <russell.matney@gmail.com>
2023-03-19 14:18:37 -0400

    feat: add logger macro to bb.edn usage

---
+++ src/logger.clj
@@ -0,0 +1,12 @@
+(ns logger)
+
+;; https://gist.github.com/borkdude/c97da85da67c7bcc5671765aef5a89ad
+(defmacro log [& msgs]
+  (let [m    (meta &form)
+        _ns  (ns-name *ns*) ;; can also be used for logging
+        file *file*]
+    `(binding [*out* *err*] ;; or bind to (io/writer log-file)
+       (println (str ~file ":"
+                     ~(:line m) ":"
+                     ~(:column m))
+                ~@msgs))))
