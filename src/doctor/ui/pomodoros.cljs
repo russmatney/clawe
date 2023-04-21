e2cc41f49b3fc8f40d745f7c89df086983101e6b
Russell Matney <russell.matney@gmail.com>
2023-04-21 10:31:15 -0400

    fix: restore pomodoro crud with time-literals
    edn/read-string requires time-literals to be passed as readers manually.

--- src/doctor/ui/pomodoros.cljs
+++ src/doctor/ui/pomodoros.cljs
@@ -2,19 +2,24 @@
   (:require
    [doctor.ui.localstorage :as localstorage]
    [clojure.edn :as edn]
+   [time-literals.read-write :as time-literals.read-write]
    [tick.core :as t]))
 
+(defn edn-read [x]
+  (edn/read-string
+    {:readers time-literals.read-write/tags}
+    x))
+
 (defn get-state []
-  nil
-  #_(or (some-> (localstorage/get-item "pomodoros")
-                edn/read-string)
-        {}))
+  (or (some-> (localstorage/get-item "pomodoros")
+              edn-read)
+      {}))
 
 (comment
   (localstorage/remove-item! "pomodoros")
-  (edn/read-string (localstorage/get-item "pomodoros"))
+  (edn-read (localstorage/get-item "pomodoros"))
   (->
-    (edn/read-string (localstorage/get-item "pomodoros"))
+    (edn-read (localstorage/get-item "pomodoros"))
     :obj :with-now)
   (localstorage/set-item! "pomodoros" (pr-str {:hi  :there
                                                :obj {:with-now (t/zoned-date-time)}})))
