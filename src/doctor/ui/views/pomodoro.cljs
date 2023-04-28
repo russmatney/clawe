0e87975dc8e27ef59a947ecfbaeea06c24e70596
Russell Matney <russell.matney@gmail.com>
2023-04-28 16:58:13 -0400

    feat: pomodoro widget consuming db data

--- src/doctor/ui/views/pomodoro.cljs
+++ src/doctor/ui/views/pomodoro.cljs
@@ -1,15 +1,12 @@
 (ns doctor.ui.views.pomodoro
   (:require
    [uix.core.alpha :as uix]
-   [doctor.ui.pomodoros :as pomodoros]
    [tick.core :as t]
-   [dates.tick :as dates]))
-
-(comment
-  (t/format
-    (t/formatter "h:mma")
-    (dates/add-tz
-      (t/zoned-date-time))))
+   [dates.tick :as dates]
+   [doctor.ui.db :as ui.db]
+   [doctor.ui.handlers :as handlers]
+   [components.debug :as debug]
+   [components.actions :as components.actions]))
 
 (def current-thresholds
   [{:label   "Go get 'em!"
@@ -28,66 +25,91 @@
     :active?   #(-> % dates/duration-since t/minutes (> 60))
     :too-long? true}])
 
-(defn widget [_opts]
-  (let [time                   (uix/state (t/zoned-date-time))
-        interval               (atom nil)
-        p-state                (pomodoros/get-state)
-        {:keys [current last]} p-state]
+(defn bar [{:keys [conn]}]
+  (let [{:keys [current last]} (ui.db/pomodoro-state conn)
+        time                   (uix/state (t/zoned-date-time))
+        interval               (atom nil)]
+    (uix/with-effect [@interval]
+      (reset! interval (js/setInterval #(reset! time (t/zoned-date-time)) 500))
+      (fn [] (js/clearInterval @interval)))
+    [:div
+     {:class ["flex flex-row" "items-center" "justify-around"]}
+
+     [:div
+      {:class ["text-slate-200" "font-nes" "mr-4"]}
+      (cond current
+            [:span
+             "Pomodoro: " (dates/human-time-since (:pomodoro/started-at current))]
+            last
+            [:span
+             "Break: "
+             (dates/human-time-since (:pomodoro/finished-at last))])]
+
+     [components.actions/actions-list (handlers/pomodoro-actions conn)]]))
+
+(defn widget [{:keys [conn] :as _opts}]
+  (let [time                               (uix/state (t/zoned-date-time))
+        interval                           (atom nil)
+        {:keys [current last] :as p-state} (ui.db/pomodoro-state conn)]
     (uix/with-effect [@interval]
       (reset! interval (js/setInterval #(reset! time (t/zoned-date-time)) 500))
       (fn [] (js/clearInterval @interval)))
 
     [:div
-     {:class ["flex flex-row" "items-center" "justify-around"
-              "bg-city-blue-700"
-              "text-city-green-200"
-              "font-mono"]}
+     {:class ["flex flex-col"]}
+     [components.actions/actions-list (handlers/pomodoro-actions conn)]
+     [:div
+      {:class ["flex flex-row" "items-center" "justify-around"
+               "bg-city-blue-700"
+               "text-city-green-200"
+               "font-mono"]}
 
-     ;; TODO pull a component out of this
-     ;; TODO genericize this thresholds thing
-     (when current
-       (let [{:keys [started-at]} current
-             active-thresholds
-             ;; TODO could make sense to have multiple active at a time
-             (->>
-               current-thresholds
-               (filter (comp #(% started-at) :active?)))]
-         [:div
-          {:class ["py-2" "px-4" "my-8"
-                   "flex flex-col" "items-center"]}
-          [:span
-           {:class ["text-4xl"]}
-           "Current"]
-          [:span
-           {:class (concat
-                     ["font-nes" "my-4" "text-2xl"]
-                     (->> active-thresholds
-                          (mapcat :class))
-                     (when (->> active-thresholds
-                                (filter :too-long?)
-                                seq)
-                       ["text-city-pink-400" "font-nes" "font-bold"
-                        "pl-2" "text-4xl" "whitespace-nowrap"]))}
-           (dates/human-time-since started-at)
-           (when (->> active-thresholds
-                      (filter :too-long?)
-                      seq)
-             "!!")]
-          (when (->> active-thresholds (filter :label) seq)
-            (for [label (->> active-thresholds (map :label))]
-              [:span {:class ["font-mono" "text-xl"]} label]))]))
+      [debug/raw-metadata {:label "state"} p-state]
 
-     (when last
-       (let [{:keys [finished-at]} last
-             latest                (:started-at current)]
-         [:div
-          {:class ["py-2" "px-4"]}
-          [:span
-           "Break: " (dates/human-time-since finished-at latest)]]))
+      ;; TODO pull a component out of this
+      ;; TODO genericize this thresholds thing
+      (when current
+        (let [{:keys [pomodoro/started-at]} current
+              active-thresholds
+              ;; TODO could make sense to have multiple active at a time
+              (->> current-thresholds
+                   (filter (comp #(% started-at) :active?)))]
+          [:div
+           {:class ["py-2" "px-4" "my-8"
+                    "flex flex-col" "items-center"]}
+           [:span
+            {:class ["text-4xl"]}
+            "Current"]
+           [:span
+            {:class (concat
+                      ["font-nes" "my-4" "text-2xl"]
+                      (->> active-thresholds
+                           (mapcat :class))
+                      (when (->> active-thresholds
+                                 (filter :too-long?)
+                                 seq)
+                        ["text-city-pink-400" "font-nes" "font-bold"
+                         "pl-2" "text-4xl" "whitespace-nowrap"]))}
+            (dates/human-time-since started-at)
+            (when (->> active-thresholds
+                       (filter :too-long?)
+                       seq)
+              "!!")]
+           (when (->> active-thresholds (filter :label) seq)
+             (for [label (->> active-thresholds (map :label))]
+               [:span {:class ["font-mono" "text-xl"]} label]))]))
 
-     (when last
-       (let [{:keys [started-at finished-at]} last]
-         [:div
-          {:class ["py-2" "px-4"]}
-          [:span
-           "Last: " (dates/human-time-since started-at finished-at)]]))]))
+      (when last
+        (let [{:keys [pomodoro/finished-at]} last
+              latest                         (:pomodoro/started-at current (dates/now))]
+          [:div
+           {:class ["py-2" "px-4"]}
+           [:span
+            "Break: " (dates/human-time-since finished-at latest)]]))
+
+      (when last
+        (let [{:pomodoro/keys [started-at finished-at]} last]
+          [:div
+           {:class ["py-2" "px-4"]}
+           [:span
+            "Last: " (dates/human-time-since started-at finished-at)]]))]]))
