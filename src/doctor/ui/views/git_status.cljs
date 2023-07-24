88e9816d49185442fb510016e0f3e076504c18fa
Russell Matney <russell.matney@gmail.com>
2023-07-24 13:01:08 -0400

    feat: display git dirty/pull/push status in table and topbar

--- src/doctor/ui/views/git_status.cljs
+++ src/doctor/ui/views/git_status.cljs
@@ -5,22 +5,41 @@
    [components.git :as components.git]
    [components.table :as components.table]
    [components.debug :as components.debug]
-   [tick.core :as t]
-   [uix.core.alpha :as uix]
    [dates.tick :as dates]
    [doctor.ui.handlers :as handlers]))
+
+(defn needs-push? [{:repo/keys [needs-push-at did-not-need-push-at]}]
+  (dates/newer needs-push-at did-not-need-push-at))
+
+(defn dirty? [{:repo/keys [dirty-at clean-at]}]
+  (dates/newer dirty-at clean-at))
+
+(defn needs-pull? [{:repo/keys [needs-pull-at did-not-need-pull-at]}]
+  (dates/newer needs-pull-at did-not-need-pull-at))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; bar
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn bar [{:keys [_conn]}]
-  [:div
-   {:class ["flex flex-row" "items-center" "justify-between" "whitespace-nowrap"]}
-   [:div
-    {:class ["text-slate-200" "font-nes" "pr-2"]}
-    "Never fetched!"]
-   [components.actions/actions-list [] #_(handlers/repo-actions conn)]])
+(defn repo-status-label [repo]
+  (str (:repo/name repo)
+       (when (dirty? repo) "#dirty")
+       (when (needs-pull? repo) "#needs-pull")
+       (when (needs-push? repo) "#needs-push")))
+
+(defn bar [{:keys [conn]}]
+  (let [repos (->> (ui.db/repos conn)
+                   (sort dirty?)
+                   (sort needs-pull?)
+                   (sort needs-push?))
+        axs   (->> repos
+                   (map (fn [repo]
+                          {:action/label    (repo-status-label repo)
+                           :action/on-click #(handlers/check-repo-status repo)
+                           ;; TODO disable/de-proritize if recently checked
+                           :action/disabled false})))]
+
+    [components.actions/actions-list axs]))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -30,15 +49,13 @@
 (defn repo-status-table-def []
   {:headers ["Repo" ":dirty?" ":needs-push?" ":needs-pull?" "Actions"]
    :->row   (fn [repo]
-              [
-               [components.debug/raw-metadata
-                {:label
-                 (components.git/short-repo repo)}
+              [[components.debug/raw-metadata
+                {:label (components.git/short-repo repo)}
                 repo]
 
-               "false"
-               "false"
-               "false"
+               (when (dirty? repo) "DIRTY!?")
+               (when (needs-push? repo) "Needs Push!?")
+               (when (needs-pull? repo) "Needs Pull!?")
 
                [components.actions/actions-list
                 {:actions (handlers/->actions repo)}]])})
@@ -54,7 +71,10 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn widget [{:keys [conn] :as _opts}]
-  (let [repos (ui.db/repos conn)]
+  (let [repos (->> (ui.db/repos conn)
+                   (sort dirty?)
+                   (sort needs-pull?)
+                   (sort needs-push?))]
     [:div
      {:class ["text-center" "my-36" "text-slate-200"]}
 
