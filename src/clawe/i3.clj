f4354c714956eb23b61f6796da4201d8c8c907c8
Russell Matney <russell.matney@gmail.com>
2023-07-24 16:15:23 -0400

    wip: i3 swap/delete wips/notes

--- src/clawe/i3.clj
+++ src/clawe/i3.clj
@@ -4,12 +4,18 @@
    [clawe.wm.protocol :refer [ClaweWM]]
    [ralphie.i3 :as r.i3]))
 
-(defn ->wsp [i3-wsp]
-  (let [title (string/replace (:i3/name i3-wsp) #"^.*: ?" "")]
-    (assoc i3-wsp
+(defn wsp->i3-wsp-name [wsp]
+  (str (:workspace/index wsp) ": " (:workspace/title wsp)))
+
+(defn i3-wsp->workspace-title [wsp]
+  (string/replace (:i3/name wsp) #"^.*: ?" ""))
+
+(defn ->wsp [wsp]
+  (let [title (i3-wsp->workspace-title wsp)]
+    (assoc wsp
            :workspace/title title
-           :workspace/index (:i3/num i3-wsp)
-           :workspace/focused (:i3/focused i3-wsp))))
+           :workspace/index (:i3/num wsp)
+           :workspace/focused (:i3/focused wsp))))
 
 (comment
   (->>
@@ -33,17 +39,24 @@
       (map ->wsp)))
 
   (-create-workspace [_this _opts workspace-title]
-    )
+    (r.i3/create-workspace workspace-title))
 
-  (-focus-workspace [_this _opts workspace])
+  (-focus-workspace [_this _opts wsp]
+    (r.i3/visit-workspace (or (:i3/num wsp) (:workspace/index wsp))))
 
-  (-fetch-workspace [_this opts workspace-title])
+  (-fetch-workspace [_this _opts workspace-title]
+    (-> workspace-title r.i3/workspace-for-name ->wsp))
 
-  (-swap-workspaces-by-index [_this a b])
+  (-swap-workspaces-by-index [_this a b]
+    ;; input is indexes? or wsps?
+    #_(r.i3/swap-workspaces a b))
 
   (-drag-workspace [_this dir])
 
-  (-delete-workspace [_this workspace])
+  (-delete-workspace [_this workspace]
+    ;; switch to this workspace, move it's contents elsewhere (scratchpad?)
+    ;; then 'kill' it
+    )
 
   ;; clients
 
@@ -58,3 +71,13 @@
   (-focus-client [_this opts client])
 
   (-move-client-to-workspace [this _opts c wsp]))
+
+(comment
+
+  (def wsps
+    (clawe.wm.protocol/-active-workspaces
+      (I3.) nil))
+
+  (clawe.wm.protocol/-swap-workspaces-by-index
+    (I3.) (first wsps) (second wsps))
+  )
--- src/ralphie/i3.clj
+++ src/ralphie/i3.clj
@@ -173,11 +173,37 @@
 ;; i3 Workspace Upsert
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(defn next-wsp-number []
+  (let [existing-wsp-nums (->> (workspaces-simple) (map :i3/num) (into #{}))]
+    (->>
+      (range 10)
+      (remove existing-wsp-nums)
+      first)))
+
+(defn create-workspace [title]
+  (let [num (next-wsp-number)]
+    (i3-msg! (str "workspace " num ": " title))))
+
 (defn visit-workspace [number]
   (i3-msg! (str "workspace number " number)))
 
 (defn rename-workspace [name number]
   (i3-msg! (str "rename workspace to " (str number ":" name))))
+
+(defn swap-workspaces [a b]
+  (let [name_a (:workspace/title a)
+        num_a  (:i3/num a)
+        name_b (:workspace/title b)
+        num_b  (:i3/num b)]
+    ;; TODO finish impling
+    (i3-msg! (str "rename workspace '" num_a ": " name_a "' to '" num_b ": " name_a "'; "
+                  "rename workspace '" num_b ": " name_b "' to '" num_a ": " name_b "'"))))
+
+(defn delete-workspace [_wsp]
+  ;; switch to this workspace, move it's contents elsewhere (scratchpad? prev-wsp?)
+  ;; then 'kill' it
+  )
+
 
 (defn upsert
   "TODO Perhaps this logic should be in workspaces?"
@@ -194,80 +220,6 @@
   (visit-workspace "4:4"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; workspaces.org items -> i3 config
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; (defn workspace->lines
-;;   [i {:keys [name props]}]
-;;   (let [{:keys [number hotkey pinned-apps]} props
-;;         number                              (or number (+ 1 i))
-;;         hotkey                              (or hotkey number)]
-;;     (concat
-;;       [(str "set $wn" number " \"" number ": " name "\"")
-;;        (str "bindsym $mod+" hotkey " workspace number $wn" number)
-;;        (str "bindsym $mod+Shift+" hotkey
-;;             " move container to workspace number $wn" number)
-;;        ;; TODO pull monitor from config
-;;        (str "workspace $wn" number " output HDMI-0 eDP-1")]
-;;       (for [app pinned-apps]
-;;         (str "for_window [class=\"" app "\"] move"
-;;              "--no-auto-back-and-forth"
-;;              "to workspace number $wn" number)))))
-
-;; (defn write-i3-ralphie
-;;   "Parses misc org data into an i3 config. Writes to the passed file."
-;;   [file]
-;;   (let [workspaces (org/fname->items "workspaces.org")]
-;;     (->> workspaces
-;;          (take 10)
-;;          (map-indexed workspace->lines)
-;;          (remove nil?)
-;;          (apply concat)
-;;          (string/join "\n")
-;;          (spit file))))
-
-;; (comment
-;;   (write-i3-ralphie (expand "~/temp-i3-conf")))
-
-;; (defn rebuild-i3-config!
-;;   "The i3 config is partially based on data in <org-dir>/workspaces.org.
-;;   This function converts that data to <i3-config-dir>/config.ralphie and
-;;   concatenates it with <i3-config-dir>/config.base.
-
-;;   Could be rewritten to not write the config.ralphie.
-;;   Left as is to help debugging/expose what this command is doing.
-;;   "
-;;   []
-;;   (let [i3-config-file (str (config/i3-dir) "/config")
-;;         base-file      (str (config/i3-dir) "/config.base")
-;;         ralphie-file   (str (config/i3-dir) "/config.ralphie")]
-;;     (write-i3-ralphie ralphie-file)
-;;     (let [base (slurp base-file)
-;;           ext  (slurp ralphie-file)]
-;;       (->> [base ext]
-;;            (string/join "\n")
-;;            (spit i3-config-file)))))
-
-;; (defn restart-i3! [] (i3-msg! "restart"))
-
-;; (defn rebuild-and-restart!
-;;   "Converts passed workspaces into an i3 config.
-;;   The contents is written to i3/config.ralphie,
-;;   which is then concattenated with i3.config.base."
-;;   [_config _parsed]
-;;   ;; TODO try-catch the rebuild, always restart
-;;   (rebuild-i3-config!)
-;;   (restart-i3!))
-
-;; (defcom rebuild-and-restart-i3
-;;   {:name          "restart-i3"
-;;    :one-line-desc "Restarts i3 in place"
-;;    :description   ["Pulls workspace config from workspaces.org."
-;;                    "Writes a new i3/config."
-;;                    "Restarts i3."]
-;;    :handler       rebuild-and-restart!})
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; resize window
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -278,11 +230,6 @@
     :i3-cmd "floating enable, resize set width 90 ppt height 90 ppt, move position center"}
    {:label  "tall-centered"
     :i3-cmd "floating enable, resize set width 40 ppt height 80 ppt, move position center"}
-   ;; TODO handle position on multiple monitors
-   ;; {:label  "right-side"
-   ;;  :i3-cmd "floating enable, resize set width 45 ppt height 90 ppt, move position center"}
-   ;; {:label  "left-side"
-   ;;  :i3-cmd "floating enable, resize set width 40 ppt height 80 ppt, move position center"}
    ])
 
 (defcom resize-window
